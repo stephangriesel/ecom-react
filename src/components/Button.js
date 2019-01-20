8ee53b8c06df1beea718cfddb89569ae7303c8ea
Stephan Griesel <sgriesel@gmail.com>
2019-01-20 16:32:45 +0100

    add navwrapper

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ButtonContainer = styled.button`
 text-transform:capitalize;
 font-size:1.4rem;
+font-family: 'PT Sans', sans-serif !important;
 background:transparent;
 border:0.05rem solid var(--lightBlue);
 color:var(--lightBlue);
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ import { ButtonContainer } from './Button';
 export default class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} width="50" alt="store" className="navbar-brand" />
         </Link>
@@ -21,13 +21,22 @@ export default class NavBar extends Component {
         <Link to="/cart" className="ml-auto">
           <ButtonContainer>
             <span className="mr-2">
-            <i className="fas fa-cart-plus">My Cart</i>
+            <i className="fas fa-cart-plus"> My Cart</i>
             </span>
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
+
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+  color:var(--mainWhite) !important;
+  font-size:1.3rem;
+  text-transform:capitalize !important;
+}
+`
 
 
