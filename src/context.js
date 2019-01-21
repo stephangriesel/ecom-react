4eb060d884ddf4b35f8cfe41f2329d134bdaabbb
Stephan Griesel <sgriesel@gmail.com>
2019-01-21 06:24:06 +0100

    test value objects

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {storeProducts, detailProduct} from './data';
 
 const ProductContext = React.createContext();
 
@@ -8,9 +9,26 @@ const ProductContext = React.createContext();
 // Consumer
 
 class ProductProvider extends Component {
+  state = {
+    products:storeProducts,
+    detailProduct: detailProduct
+  }
+
+  handleDetail = () => {
+    console.log('hello from detail');
+  }
+
+  addToCart = () => {
+    console.log('hello from detail');
+  }
+
   render() {
     return (
-      <ProductContext.Provider value="Hello from context">
+      <ProductContext.Provider value={{
+        ...this.state,
+        handleDetail: this.handleDetail,
+        addToCart: this.addToCart
+      }}>
         {this.props.children}
       </ProductContext.Provider>
     )
