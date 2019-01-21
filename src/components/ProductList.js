7621e27afe3b1a24555328e3f5682f696612b332
Stephan Griesel <sgriesel@gmail.com>
2019-01-21 05:35:08 +0100

    update produclist component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react'
-import Product from './Product';
+import React, { Component } from "react";
+import Product from "./Product";
 
 export default class ProductList extends Component {
+  state = {
+    products: []
+  };
   render() {
     return (
-      <div>
-        <Product></Product>
-      </div>
-    )
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <div className="row" />
+          </div>
+        </div>
+      </React.Fragment>
+      // <Product></Product>
+    );
   }
 }
