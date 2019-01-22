a2492866e154ad01e4b552026b150070561b7af3
Stephan Griesel <sgriesel@gmail.com>
2019-01-22 06:23:03 +0100

    update product footer

--- src/components/Product.js
+++ src/components/Product.js
@@ -16,10 +16,28 @@ export default class Product extends Component {
             <Link to="/details">
               <img src={img} alt="product" className="card-img-top" />
             </Link>
-            <button className="cart-btn" disabled={inCart ? true : false} onClick={() => {console.log('added to cart');
-          }}>
-          {inCart?(<p className="text-capitalize mb-0" disabled>inCart</p>):(<i className="fas fa-cart-plus" />)}
-          </button>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                console.log("added to cart");
+              }}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  In Cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
+          </div>
+          {/* Product Footer */}
+          <div className="card-footer d-flex justify-content-between">
+          <p className="align-self-center mb-0">{title}</p>
+          <h5 className="text-blue font-italic mb-0">
+          <span className="mr-1">$</span>{price}
+          </h5>
           </div>
         </div>
       </ProductWrapper>
