51809b476ae88edba75acbf165cef321b9d076fb
Stephan Griesel <sgriesel@gmail.com>
2019-01-24 06:26:02 +0100

    update details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
 
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from Details</h3>
-      </div>
-    )
+      <ProductConsumer>
+        {value => {
+          const {
+            id,
+            company,
+            img,
+            info,
+            price,
+            title,
+            inCart
+          } = value.detailProduct;
+          return (
+            <div className="container py-5">
+              {/* Title */}
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              {/* Title End */}
+              {/* Product Info */}
+              <div className="row">
+                {/* Product Image */}
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img src={img} className="img-fluid" alt="product" />
+                </div>
+                {/* Product Text */}
+                <div className="col-10 mx-auto col-md-6 my-3">
+                <h2>Model: {title}</h2>
+                <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                Made by: <span className="text-uppercase">{company}</span>
+                </h4>
+                <h4 className="text-blue">
+                Price: <span>$</span>{price}
+                </h4>
+                <p className="text-capitalize font-weight-bold mt-3 mb-0">More info about the product: 
+                </p>
+                {info}
+                </div>
+              </div>
+              {/* Product Info End */}
+            </div>
+          );
+        }}
+      </ProductConsumer>
+    );
   }
 }
